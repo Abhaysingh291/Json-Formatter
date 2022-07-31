3d71b5748873da5e2e84916af9bc7e46df6ab431
Abhaysingh291 <87523630+Abhaysingh291@users.noreply.github.com>
2022-07-31 05:32:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const inputArea = document.querySelector(".large-area--input");
-const outputArea = document.querySelector(".large-area--output");
 const btnFormat = document.querySelector(".button--format");
 const btnClear = document.querySelector(".button--clear");
+
+function stringify(obj) {
+  if (typeof obj === "string") {
+    return `"${obj}"`;
+  }
+
+  if (typeof obj === "number" || typeof obj === "boolean") {
+    return `${obj}`;
+  }
+
+  if (Array.isArray(obj)) {
+    let res = "[";
+    for (let i = 0; i < obj.length; i++) {
+      res += `${stringify(obj[i])},`;
+    }
+
+    res = `${res.substring(0, res.length - 1)}]`;
+    return res;
+  }
+
+
+  let res = `{`;
+  for (let key in obj) {
+    res += `"${key}":${stringify(obj[key])},`;
+  }
+
+  res = `${res.substring(0, res.length - 1)}}`;
+  return res;
+}
 
 btnFormat.addEventListener("click", () => {
     const str = inputArea.value;
     function isJSON(text) {
         if (0 == text.trim().length) return outputArea.value = "error Sorry, Input is Empty.";
         try {
-          const e=JSON.stringify(JSON.parse(inputArea.value), null, 4);
+            const e =stringify(JSON.parse(inputArea.value,null,4));
+            // console.log(JSON.parse(stringifyJSON(inputArea.value)))
+            // const e =JSON.parse(stringifyJSON(str));
             outputArea.value = e;
             outputArea.style.color = "green";
         }
-        catch (e) {
-            console.log(e);
-            outputArea.style.color = "red";
-            return outputArea.value = (e.message)
-        }
-    }
-    isJSON(str);
-});
-
-btnClear.addEventListener("click", () => {
-    inputArea.value = "";
+	@@ -57,20 +26,3 @@ btnClear.addEventListener("click", () => {
     outputArea.value = "";
 
 })
